ed3e4df4bf5b0c881d831e28c9f64f0f34561f45
Mimi Cong <mina123fire@yahoo.com>
2020-08-25 17:52:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
 var playlist={artist1:"song1", artist2:"song2"}
 
 function updatePlaylist (playlist, artistName, songTitle) {
-  playlist.artistNamet=songTitle;
+  playlist[artistNamet]=songTitle;
   return playlist;
 }
 
 function removeFromPlaylist (playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
 }
